e4a46a5f5d268bd352f6ffcf0d6f5a122f95d76b
Andrey Kozlov <andrew08552gmail.com>
2023-03-23 15:11:04 +0300

    made appearance and closing of the popup

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 "use strict";
 
 
-
+document.querySelector(".main__btn").addEventListener("click",()=>{
+    document.querySelector(".popup").style.visibility = 'visible';
+    document.querySelector(".popup").style.opacity    = "1";
+})
+document.querySelector(".popup__close").addEventListener("click", ()=>{
+    document.querySelector(".popup").style.visibility = 'hidden';
+    document.querySelector(".popup").style.opacity    = "0";
+})
+document.querySelector(".form__btn").addEventListener("click", ()=>{
+    document.querySelector(".popup").style.visibility = 'hidden';
+    document.querySelector(".popup").style.opacity    = "0";
+})
